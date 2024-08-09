598abbe556be8c709084320b4d716eecdc2fffe8
T0madon <joao.daciuk@gmail.com>
2024-08-09 11:33:40 -0300

    09_08

--- section4/aula84.py
+++ section4/aula84.py
@@ -11,4 +11,17 @@ lista = [
     numero * 2
     for numero in range(10)
 ]
-print(lista)
+# print(lista)
+
+produtos = [
+    {"nome": 'p1', 'preco': 20,},
+    {'nome': 'p2', 'preco': 10,},
+    {'nome': 'p3', 'preco': 30,},
+]
+
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    for produto in produtos
+]
+
+print(*novos_produtos, sep='\n')
