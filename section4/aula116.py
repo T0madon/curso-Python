3c13b361f0e2373d385c3bbe515741a77996a534
T0madon <joao.daciuk@gmail.com>
2024-12-08 18:47:10 -0300

    last

--- section4/aula116.py
+++ section4/aula116.py
@@ -26,23 +26,30 @@ caminho_arquivo = '.\\section4\\aula116.txt'
 # arquivo = open(caminho_arquivo, 'w')
 # arquivo.close()
 
-with open(caminho_arquivo, 'w+') as arquivo:
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0,0)
+#     print(arquivo.read())
+
+
+#     print('READLINES')
+#     arquivo.seek(0,0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+# print('#' * 10 + '\n')
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+with open(caminho_arquivo, 'a+') as arquivo:
     arquivo.write('Linha 1\n')
     arquivo.write('Linha 2\n')
     arquivo.writelines(
         ('Linha 3\n', 'Linha 4\n')
     )
-    arquivo.seek(0,0)
-    print(arquivo.read())
-
-
-    print('READLINES')
-    arquivo.seek(0,0)
-    for linha in arquivo.readlines():
-        print(linha.strip())
-
-
-print('#' * 10 + '\n')
-
-# with open(caminho_arquivo, 'r') as arquivo:
-#     print(arquivo.read())
