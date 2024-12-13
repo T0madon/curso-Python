f91ec852fab8ecfb4a96beac54e29528127e64bd
T0madon <joao.daciuk@gmail.com>
2024-12-13 11:03:45 -0300

    13_12

---
+++ section5/aula127a-ex-json.py
@@ -0,0 +1,15 @@
+# Exercício - Salve sua classe em JSON
+# Salve os dados da sua classe em JSON
+# e depois crie novamente as instâncias
+# da classe com os dados salvos
+# Faça em arquivos separados.
+
+CAMINHO = 'aula127-dados.json'
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+        
