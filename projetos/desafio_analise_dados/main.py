8bd4588b18388ff9e3d31c1decd41c32a4d5d8c6
T0madon <joao.daciuk@gmail.com>
2025-01-06 14:11:20 -0300

    exceptions

--- projetos/desafio_analise_dados/main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Carregar o dataset caminho_arquivo = '.\\section4\\aula116.txt'
-file_path = 'C:\\Users\\joaod\\OneDrive\\Documentos\\Curso Python\\projetos\\desafio_analise_dados\\Dataset.csv'
-data = pd.read_csv(file_path)
-
-# Visualizar as primeiras linhas do dataset
-data.head()
