5371cbf9c9cd1b78c838fa2ef1b1d47f219dfac4
T0madon <joao.daciuk@gmail.com>
2025-01-12 12:25:11 -0300

    class_pessoa

--- section5/aula158/conta.py
+++ section5/aula158/conta.py
@@ -1,31 +1,31 @@
 from abc import ABC, abstractmethod
 
 class Conta(ABC):
-    def __init__(self, agency, number, balance=0):
+    def __init__(self, agency: int, number: int, balance: float =0) -> None :
         self.agency = agency
         self.number = number
         self.balance = balance
 
     @abstractmethod
-    def take(self, value):
+    def take(self, value: float) -> float:
         ...
 
-    def deposit(self, value):
+    def deposit(self, value: float) -> float:
         self.balance += value
         self.details(f'(DEPÓSITO DE R${value:.2f})')
+        return self.balance
 
-    def details(self, msg=''):
+    def details(self, msg: str ='') -> None:
         print(f'{msg}\nO seu saldo é de R${self.balance:.2f} ')
         print('-----------')
 
 class ContaCorrente(Conta):
-    def __init__(self, agency, number, balance=0, lim=0):
+    def __init__(self, agency: int, number: int, balance=0, lim=0):
         super().__init__(agency, number, balance)
         self.lim = lim
 
 
     def take(self, value):
-
         if (self.balance - value) >= -self.lim:
             self.balance -= value
             self.details(f'SAQUE DE R${value:.2f}')
@@ -33,6 +33,7 @@ class ContaCorrente(Conta):
         
         print(f'Não foi possível sacar o valor de R${value}')
         self.details()
+        return self.balance
 
 
 class ContaPoupanca(Conta):
--- section5/aula158/pessoa.py
+++ section5/aula158/pessoa.py
@@ -1,22 +1,36 @@
-from conta import Conta, ContaCorrente, ContaPoupanca
+from conta import ContaCorrente, ContaPoupanca
 
 class Pessoa:
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
 
     @property
     def name(self):
-        return self.name
+        return self._name
+    
+    @name.setter
+    def name(self, value: str):
+        self._name = value
     
     @property
     def age(self):
         return self.age
     
+    @age.setter
+    def age(self, value: int):
+        self._age = value
+    
 class Cliente(Pessoa):
-    def __init__(self, name, age, cc:  = None, cp = None):
+    def __init__(self, name: str, age: int) -> None:
         super().__init__(name, age)
-        self.cc = cc
-        self.cp = cp
+        self.account = []
+
+    def add_cc(self, cc: ContaCorrente):
+        self.account[0] = cc 
+
+    def add_cp(self, cp: ContaPoupanca):
+        self.account[1] = cp 
+
 
     
