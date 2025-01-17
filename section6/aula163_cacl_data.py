91bf69898f09cd5623fecebc815e6efac3f83b99
T0madon <joao.daciuk@gmail.com>
2025-01-17 12:54:06 -0300

    exercicio

--- section6/aula163_cacl_data.py
+++ section6/aula163_cacl_data.py
@@ -9,6 +9,9 @@ fmt = '%d/%m/%Y %H:%M:%S'
 data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
 data_fim = datetime.strptime('12/12/2022 08:20:20', fmt)
 # delta = timedelta(days=10, hours=2)
+
+print(data_fim)
+
 delta = relativedelta(data_fim, data_inicio)
 print(delta.days, delta.years)
 # print(data_fim - delta)
