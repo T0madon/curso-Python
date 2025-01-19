2065f6a6775d4f364916e51176bf64fe46229649
T0madon <joao.daciuk@gmail.com>
2025-01-18 23:13:48 -0300

    json-dump-load

---
+++ section6/aula177_jsonLoad&Dump.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+NOME_ARQUIVO = 'aula177.json'
+
+CAMINHO_ABS_ARQUIVO = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        NOME_ARQUIVO
+    )
+) 
+
+filme = {
+    'title': 'O Senhor dos Anéis: A Sociedade do Anel',
+    'original_title': 'The Lord of the Rings: The Fellowship of the Ring',
+
+    'is_movie': True,
+    'imdb_rating': 8.8,
+    'year': 2001,
+    'characters': ['Frodo', 'Sam', 'Gandalf', 'Legolas', 'Boromir'],
+    'budget': None
+}
+
+with open(CAMINHO_ABS_ARQUIVO, 'w') as arquivo:
+    json.dump(filme, arquivo, ensure_ascii=False, indent=2)
+    
+with open(CAMINHO_ABS_ARQUIVO, 'r') as arquivo:
+    filme_do_json = json.load(arquivo)
+    print(filme_do_json)
